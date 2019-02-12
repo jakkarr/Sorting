4de09132e6366dc01754f324896e2ce9521c230b
16329 <marcel.weitzel@nordakademie.de>
2019-02-12 16:02:29 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Main m = new Main();
+            int[] random = m.createRandomArray(100_000_000);
+
+        long start= System.nanoTime();
+        MergeSort sort = new MergeSort();
+        sort.mergeSort(random);
+        System.out.println("fertig\t"+((System.nanoTime()-start)/1_000_000f+" ms"));
+    }
+    
+    public int[] createRandomArray(int size){
+        Random r = new Random(System.currentTimeMillis());
+        int[] retval = new int[size];
+            for (int i = 0; i < size; i++) {
+                retval[i] = r.nextInt(size);
+            }
+        return retval;
+    }
+    
+
+
+    public void show(int[] a){
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < a.length; i++) {
+            list.add(a[i]);
+        }
+        System.out.println(list);
+    }
+}
---
+++ src/MergeSort.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+public class MergeSort {
+    public int[] mergeSort(int[] list){
+        if(list.length>1) {
+            int[] firsthalf = Arrays.copyOfRange(list, 0, (list.length / 2));
+            int[] secondhalf = Arrays.copyOfRange(list, list.length / 2, list.length);
+            return merge(mergeSort(firsthalf), mergeSort(secondhalf));
+        }else {
+            return list;
+        }
+    }
+
+    private int[] merge(int[] a, int[] b){
+        int[] retval = new int[a.length+b.length];
+        int apos = 0;
+        int bpos = 0;
+        for (int i = 0; i < retval.length; i++) {
+            if (apos>=a.length){
+                if (bpos>= b.length){
+                    return retval;
+                }else{
+                    retval = copy(b, retval, b.length-bpos);
+                    return retval;
+                }
+            }else{
+                if (bpos>= b.length){
+                    copy(a, retval, a.length-apos);
+                }else{
+                    if (a[apos]<= b[bpos]){
+                        retval[i] = a[apos];
+                        apos++;
+                    }else{
+                        retval[i] = b[bpos];
+                        bpos++;
+                    }
+                }
+            }
+
+        }
+        return retval;
+    }
+
+    private int[] copy(int[] thisArray, int[] intoThis, int remainingBits){
+        for (int i = 0; i < remainingBits; i++) {
+            intoThis[intoThis.length-i-1] = thisArray[thisArray.length-i-1];
+        }
+        return intoThis;
+    }
+}
